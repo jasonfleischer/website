16e47e51fb5128c3479f4078971151c222cb45cb
Jason Fleischer <jfleischer@hcn-inc.com>
2016-03-15 22:20:30 -0400

    revamped

--- js/main.js
+++ js/main.js
@@ -10,8 +10,9 @@ window.onload = function(){
 	}
 }
 function homePage(){
+	window.scrollTo(0, 0);
 	document.getElementById("resumeM").className = "";
-	document.getElementById("emailM").className = "";
+	document.getElementById("git").className = "";
 	document.getElementById("fbM").className = "";
 	document.getElementById('personalInfo').style.display = 'block';
 	document.getElementById('resume').style.display = '';
@@ -20,32 +21,24 @@ function homePage(){
 }
 
 function resumePage(){	
+	window.scrollTo(0, 0);
 	document.getElementById("resumeM").className = "active";
-	document.getElementById("emailM").className = "";
+	document.getElementById("git").className = "";
 	document.getElementById("fbM").className = "";
 	document.getElementById('personalInfo').style.display = 'none';
 	document.getElementById('resume').style.display = 'block';
 }
-function email(){
+function git(){
 	document.getElementById("resumeM").className = "";
-	document.getElementById("emailM").className = "active";
+	document.getElementById("git").className = "active";
 	document.getElementById("fbM").className = "";
 }
 function openFaceBook(){
 	document.getElementById("resumeM").className = "";
-	document.getElementById("emailM").className = "";
+	document.getElementById("git").className = "";
 	document.getElementById("fbM").className = "active";
 }
 function googleSearch(){
 	if(document.getElementById('q').value.length>0)
 		window.open("http://www.google.com/search?q="+document.getElementById('q').value);
-}
-function toggleBio(){
-	if(document.getElementById('bioContent').style.display != 'none'){
-		document.getElementById('bioHeader').innerHTML = 'Click here to see more information';		
-		document.getElementById('bioContent').style.display = 'none';
-	}else{
-		document.getElementById('bioHeader').innerHTML = 'About myself:';		
-		document.getElementById('bioContent').style.display = 'block';			
-	}
 }
