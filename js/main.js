128f60a19075f7b52137c3474af5a31b23ce6254
Jason Fleischer <jfleischer@Jasons-MacBook-Pro.local>
2019-10-16 16:17:20 -0400

    website up

--- js/main.js
+++ js/main.js
@@ -2,28 +2,12 @@
 function homePage(){
 	window.scrollTo(0, 0);
 	document.getElementById("resumeM").className = "";
-	document.getElementById("git").className = "";
-	//document.getElementById("fbM").className = "";
 	document.getElementById('personalInfo').style.display = 'block';
-	document.getElementById('resume').style.display = '';
-	document.getElementById('bioHeader').innerHTML = 'About myself:';		
-	document.getElementById('bioContent').style.display = 'block';	
+	document.getElementById('resume').style.display = 'none';		
 }
 function resumePage(){	
 	window.scrollTo(0, 0);
 	document.getElementById("resumeM").className = "active";
-	document.getElementById("git").className = "";
-	//document.getElementById("fbM").className = "";
 	document.getElementById('personalInfo').style.display = 'none';
 	document.getElementById('resume').style.display = 'block';
-}
-function git(){
-	//document.getElementById("resumeM").className = "";
-	//document.getElementById("git").className = "active";
-	//document.getElementById("fbM").className = "";
-}
-function openFaceBook(){
-	document.getElementById("resumeM").className = "";
-	document.getElementById("git").className = "";
-	//document.getElementById("fbM").className = "active";
 }
