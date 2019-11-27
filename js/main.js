1ffad2a6f70b0ea2578541cc4714dcabe6f0500e
Jason Fleischer <jfleischer@Jasons-MacBook-Pro.local>
2019-11-27 15:53:43 -0500

    bootstrap removed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-function homePage(){
-	window.scrollTo(0, 0);
-	document.getElementById("resumeM").className = "";
-	document.getElementById('personalInfo').style.display = 'block';
-	document.getElementById('resume').style.display = 'none';		
-}
-function resumePage(){	
-	window.scrollTo(0, 0);
-	document.getElementById("resumeM").className = "active";
-	document.getElementById('personalInfo').style.display = 'none';
-	document.getElementById('resume').style.display = 'block';
-}
+
